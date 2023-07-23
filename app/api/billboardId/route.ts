28994cded8e0958165e6b45d83bf49f633937807
Marian <pidchashymaryan@gmail.com>
2023-07-23 12:58:49 +0300

    fix: billboardId get

---
+++ app/api/billboardId/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+import axios from 'axios';
+
+import prismaClient from '@/lib/db';
+
+export const GET = async () => {
+  try {
+    const { data: storeId } = await axios.get(
+      `${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/storeId`
+    );
+
+    const billboard = await prismaClient.category.findFirst({
+      where: {
+        storeId
+      }
+    });
+
+    const billboardId = billboard?.billboardId;
+
+    return NextResponse.json(billboardId, { status: 200 });
+  } catch (err: unknown) {
+    console.log('[STOREID_GET]', err);
+    return new NextResponse('Internal errro', { status: 500 });
+  }
+};
