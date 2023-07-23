70c2276a9ea5ceb95f09e29b29fac47b5cacdd60
Marian <pidchashymaryan@gmail.com>
2023-07-23 12:16:31 +0300

    feat: storeId get

---
+++ app/api/storeId/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+
+import prismaClient from '@/lib/db';
+
+export const GET = async () => {
+  try {
+    const userId = 'user_2RtGt5quTPApMsgzfdO7Ss8yZ8a';
+
+    const store = await prismaClient.store.findFirst({
+      where: {
+        userId
+      }
+    });
+
+    const storeId = store?.id;
+
+    return NextResponse.json(storeId, { status: 200 });
+  } catch (err: unknown) {
+    console.log('[STOREID_GET]', err);
+    return new NextResponse('Internal errro', { status: 500 });
+  }
+};
