f8b0152edfe938f9dbf18909547a0d5475b23e4d
Pidchashyi Marian <pidchashymaryan@gmail.com>
2023-10-09 21:48:46 +0300

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const ADMIN_USER_ID = 'user_2RtGt5quTPApMsgzfdO7Ss8yZ8a'; // Hard coded (My Id)
 
 export const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://stores.marian1309.vercel.app',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 };
