dc374ccfe580b6637fa8d497201988b9c408881f
Marian <pidchashymaryan@gmail.com>
2023-10-09 21:25:06 +0300

    refactor: idk

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,5 @@ export const ADMIN_USER_ID = 'user_2RtGt5quTPApMsgzfdO7Ss8yZ8a'; // Hard coded (
 export const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  Authorization:
-    'Bearer sk_test_51NWPY6F0XQexAIWYhsNuHZQtfEH75soEzzyKnZo4grbkb2ojxSePgYYRc69lD9qco5Rzz4m5ZhIZpFpDSiVN9a6s00i2fAut6O'
+  'Access-Control-Allow-Headers': 'Authorization'
 };
